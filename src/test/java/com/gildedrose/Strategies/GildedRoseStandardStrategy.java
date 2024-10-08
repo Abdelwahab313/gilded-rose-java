d0a6f10ee9caff7cd182dda742de66b20941b423
Abdelwahab <Abdelwahabahmed93@gmail.com>
2024-10-08 23:01:40 +0300

    Add extra tests for standard items

--- src/test/java/com/gildedrose/Strategies/GildedRoseStandardStrategy.java
+++ src/test/java/com/gildedrose/Strategies/GildedRoseStandardStrategy.java
@@ -12,4 +12,20 @@ class GildedRoseStandardStrategyTest {
         assertEquals(19, item.quality);
         assertEquals(9, item.sellIn);
     }
+
+    @Test
+    void qualityDegradesTwiceFastAfterSellIn() {
+        Item item = new Item("Standard Item", 0, 20, new StandardItemStrategy());
+        item.updateQuality();
+        assertEquals(18, item.quality);
+        assertEquals(-1, item.sellIn);
+    }
+
+    @Test
+    void qualityIsNeverNegative() {
+        Item item = new Item("Standard Item", 5, 0, new StandardItemStrategy());
+        item.updateQuality();
+        assertEquals(0, item.quality);
+        assertEquals(4, item.sellIn);
+    }
 }
