8a62f7fbb7cc7cee8b77e4a24b2007df69672a1a
Abdelwahab <Abdelwahabahmed93@gmail.com>
2024-10-08 23:05:19 +0300

    Add extra tests for Aged Brie items

---
+++ src/main/java/com/gildedrose/Strategies/AgedBrieStrategy.java
@@ -0,0 +1,16 @@
+package com.gildedrose.Strategies;
+
+import com.gildedrose.Item;
+
+class AgedBrieStrategy implements QualityUpdateRule {
+    @Override
+    public void updateQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+            if (item.sellIn <= 0 && item.quality < 50) {
+                item.quality++;
+            }
+        }
+        item.sellIn--;
+    }
+}
---
+++ src/test/java/com/gildedrose/Strategies/GildedRoseAgedBrieStrategyTest.java
@@ -0,0 +1,33 @@
+package com.gildedrose.Strategies;
+
+import com.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseAgedBrieStrategyTest {
+    @Test
+    void agedBrieIncreasesInQuality() {
+        Item item = new Item("Aged Brie", 2, 0, new AgedBrieStrategy());
+        item.updateQuality();
+        assertEquals(1, item.quality);
+        assertEquals(1, item.sellIn);
+    }
+
+    @Test
+    void agedBrieQualityNeverExceeds50() {
+        Item item = new Item("Aged Brie", 2, 50, new AgedBrieStrategy());
+        item.updateQuality();
+        assertEquals(50, item.quality);
+        assertEquals(1, item.sellIn);
+    }
+
+    @Test
+    void agedBrieIncreasesQualityTwiceAsFastAfterSellIn() {
+        Item item = new Item("Aged Brie", 0, 0, new AgedBrieStrategy());
+        item.updateQuality();
+        assertEquals(2, item.quality);
+        assertEquals(-1, item.sellIn);
+    }
+
+}
