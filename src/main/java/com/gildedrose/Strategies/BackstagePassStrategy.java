6e0678708ebbaaedf61d92d4dd790f6935d43011
Abdelwahab <Abdelwahabahmed93@gmail.com>
2024-10-08 23:23:24 +0300

    Add extra tests for Backstage pass items

--- src/main/java/com/gildedrose/Strategies/BackstagePassStrategy.java
+++ src/main/java/com/gildedrose/Strategies/BackstagePassStrategy.java
@@ -5,8 +5,18 @@ import com.gildedrose.Item;
 class BackstagePassStrategy implements QualityUpdateRule {
     @Override
     public void updateQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
+        if (item.sellIn <= 0) {
+            item.quality = 0;
+        } else {
+            if (item.quality < 50) {
+                item.quality++;
+                if (item.sellIn <= 10) {
+                    if (item.quality < 50) item.quality++;
+                }
+                if (item.sellIn <= 5) {
+                    if (item.quality < 50) item.quality++;
+                }
+            }
         }
         item.sellIn--;
     }
--- src/test/java/com/gildedrose/Strategies/BackstagePassStrategyTest.java
+++ src/test/java/com/gildedrose/Strategies/BackstagePassStrategyTest.java
@@ -14,4 +14,35 @@ class BackstagePassStrategyTest {
         assertEquals(14, item.sellIn);
     }
 
+    @Test
+    void qualityIncreasesByTwoIfSellInLessThanOrEqualTo10() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20, new BackstagePassStrategy());
+        item.updateQuality();
+        assertEquals(22, item.quality);
+        assertEquals(9, item.sellIn);
+    }
+
+    @Test
+    void qualityIncreasesByThreeIfSellInLessThanOrEqualTo5() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20, new BackstagePassStrategy());
+        item.updateQuality();
+        assertEquals(23, item.quality);
+        assertEquals(4, item.sellIn);
+    }
+
+    @Test
+    void qualityDropsToZeroAfterConcert() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20, new BackstagePassStrategy());
+        item.updateQuality();
+        assertEquals(0, item.quality);
+        assertEquals(-1, item.sellIn);
+    }
+
+    @Test
+    void qualityNeverExceeds50() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49, new BackstagePassStrategy());
+        item.updateQuality();
+        assertEquals(50, item.quality);
+        assertEquals(4, item.sellIn);
+    }
 }
